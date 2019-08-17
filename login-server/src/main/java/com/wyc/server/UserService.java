32f86e80d146ba0be12a862dce38b5c4fe6196f8
wangyanci <1973155911@qq.com>
2019-08-17 18:14:49 +0800

    --2019/8/17  18：14  切换mysql和redis实例地址

--- login-server/src/main/java/com/wyc/server/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.wyc.server;
-
-import com.wyc.entity.OrgUser;
-
-import java.util.List;
-
-public interface UserService {
-    List<OrgUser> queryUserAll(Integer pageNum,Integer pageSize);
-}
